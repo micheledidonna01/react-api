95ed77304157bd4b7e93a7002fa54f865f82b15b
micheledidonna01 <micheledidonna337@gmail.com>
2025-04-11 17:18:08 +0200

    feat: milestone 3

--- src/App.jsx
+++ src/App.jsx
@@ -22,20 +22,35 @@ function App() {
   console.log(authors);
   return (
     <>
-      <div className="contenitore d-flex flex-wrap m-5">
+      <div className="contenitore d-flex flex-wrap my-5 mx-auto p-3">
         {authors.map((author) =>
-          <div key={author.id} className='card d-flex col col-4'>
-            <img src={author.image} alt={author.name} className='card-img-top' />
-            <ul className='list-group list-group-flush'>
-              <li className="list-group-item">Nome: <strong>{author.name}</strong></li>
-              <li className="list-group-item">Anno di nascita: <strong>{author.birth_year}</strong></li>
-              <li className="list-group-item">Nazionalità: <strong>{author.nationality}</strong></li>
-              <li className="list-group-item">Biografia: <strong>{author.biography}</strong></li>
-              <li className="list-group-item">Riconoscimenti: <strong>{author.awards}</strong></li>
-            </ul>
+          // // <div key={author.id} className='sotto-contenitore d-flex col-12 align-items-center'>
+          // //   <div className="image col-4">
+          // //     <img src={author.image} alt={author.name} className='card-img-left'/>
+          // //   </div>
+          // //   <ul className='list-group list-group-flush col-8'>
+          // //     <li className="list-group-item list-group-item-action list-group-item-primary">Nome: <strong>{author.name}</strong></li>
+          // //     <li className="list-group-item list-group-item-action list-group-item-secondary">Anno di nascita: <strong>{author.birth_year}</strong></li>
+          // //     <li className="list-group-item list-group-item-action list-group-item-success">Nazionalità: <strong>{author.nationality}</strong></li>
+          // //     <li className="list-group-item list-group-item-action list-group-item-info">Biografia: <strong>{author.biography}</strong></li>
+          // //     <li className="list-group-item list-group-item-action list-group-item-warning">Riconoscimenti: <strong>{author.awards}</strong></li>
+          // //   </ul>
 
 
+          // </div>
+          <div className="card">
+            <img src={author.image} className="card-img-top" alt={author.title}/>
+              <div className="card-body">
+                <h5 className="card-title">{author.name}</h5>
+                <p className="card-text">{author.biography}</p>
+              </div>
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item"><strong>Nationality: </strong>{author.nationality}</li>
+              <li className="list-group-item"><strong>Birth year: </strong>{author.birth_year}</li>
+              <li className="list-group-item"><strong>Awards: </strong>{author.awards}</li>
+              </ul>
           </div>
+          
         )}
       </div>
 

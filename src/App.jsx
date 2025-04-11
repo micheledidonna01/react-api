bc107cf359171b2863dfe36cc48745bfc96a467d
micheledidonna01 <micheledidonna337@gmail.com>
2025-04-11 16:29:22 +0200

    feat: milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-
-import axios from 'axios'
 import { useEffect, useState } from 'react';
+import axios from 'axios'
+
 
 
 function App() {
@@ -12,21 +12,30 @@ function App() {
     axios.get(api)
       .then((res) => {
         setAuthors(res.data);
-  
+
       })
       .catch(err => console.log(err))
   }
 
-  
+
   useEffect(fetchAuthors, []);
   console.log(authors);
   return (
     <>
-      <div className="container">
-        {authors.map((author) => {
-          <div key={author.id} className='card'>
-          {author.name}
-          </div>}
+      <div className="contenitore d-flex flex-wrap m-5">
+        {authors.map((author) =>
+          <div key={author.id} className='card d-flex col col-4'>
+            <img src={author.image} alt={author.name} className='card-img-top' />
+            <ul className='list-group list-group-flush'>
+              <li className="list-group-item">Nome: <strong>{author.name}</strong></li>
+              <li className="list-group-item">Anno di nascita: <strong>{author.birth_year}</strong></li>
+              <li className="list-group-item">Nazionalità: <strong>{author.nationality}</strong></li>
+              <li className="list-group-item">Biografia: <strong>{author.biography}</strong></li>
+              <li className="list-group-item">Riconoscimenti: <strong>{author.awards}</strong></li>
+            </ul>
+
+
+          </div>
         )}
       </div>
 

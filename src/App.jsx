3545ac22cd3a9a1e426da2144019fac17b33ddbc
micheledidonna01 <micheledidonna337@gmail.com>
2025-04-11 15:33:21 +0200

    feat: Milestone 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
 
-import './App.css'
+import axios from 'axios'
+import { useEffect, useState } from 'react';
+
 
 function App() {
 
+  let api = "https://www.freetestapi.com/api/v1/actors";
+  const [authors, setAuthors] = useState([]);
 
+  function fetchAuthors() {
+    axios.get(api)
+      .then((res) => {
+        setAuthors(res.data);
+  
+      })
+      .catch(err => console.log(err))
+  }
+
+  
+  useEffect(fetchAuthors, []);
+  console.log(authors);
   return (
     <>
+      <div className="container">
+        {authors.map((author) => {
+          <div key={author.id} className='card'>
+          {author.name}
+          </div>}
+        )}
+      </div>
 
     </>
   )
